e3560aac8adbb57cc7c1b72fe98473caedc8fda1
Artyom Shukanov <artyom_shukanov@epam.com>
2017-08-03 18:39:48 +0300

    Initial commit for creating ID4 prototype from scratch

---
+++ Prototypes/IdentityProvider/Config.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace IdentityProvider
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
