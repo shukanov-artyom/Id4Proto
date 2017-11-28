5a1aceb9706aa27ce410a439077e6a255f9c2018
Artyom Shukanov <shukanov.artyom@gmail.com>
2017-11-28 13:03:50 +0300

    additional client

---
+++ Prototypes/id4-WebForms-Implicit/Medbullets/Logout.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web;
+
+namespace WebApp
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Request.GetOwinContext().Authentication.SignOut();
+        }
+    }
+}
---
+++ Prototypes/id4-WebForms-Implicit/Medbullets/Startup.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Extensions;
+using Owin;
+using System.IdentityModel.Tokens;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Linq;
+using System.Security.Claims;
+using IdentityModel.Client;
+using Microsoft.IdentityModel.Protocols;
+
+[assembly: OwinStartup(typeof(WebApp.Startup))]
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                AuthenticationType = "Cookies",
+                ExpireTimeSpan = TimeSpan.FromMinutes(10),
+                SlidingExpiration = true
+            });
+
+            JwtSecurityTokenHandler.InboundClaimTypeMap.Clear();
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                AuthenticationType = "oidc",
+                SignInAsAuthenticationType = "Cookies",
+                Authority = "https://localhost:44336/",
+                ClientId = "implicit.medbullets",
+                RedirectUri = "http://localhost:5969/",
+                PostLogoutRedirectUri = "http://localhost:5969/",
+                ResponseType = "id_token token",
+                Scope = "openid profile email address",
+                UseTokenLifetime = false,
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    SecurityTokenValidated = async n =>
+                    {
+                        var claimsToExclude = new[]
+                        {
+                            "aud", "iss", "nbf", "exp", "nonce", "iat", "at_hash"
+                        };
+
+                        var claimsToKeep =
+                            n.AuthenticationTicket.Identity.Claims
+                            .Where(x => false == claimsToExclude.Contains(x.Type)).ToList();
+                        claimsToKeep.Add(new Claim("id_token", n.ProtocolMessage.IdToken));
+
+                        if (n.ProtocolMessage.AccessToken != null)
+                        {
+                            claimsToKeep.Add(new Claim("access_token", n.ProtocolMessage.AccessToken));
+
+                            var userInfoClient = new UserInfoClient(
+                                new Uri("https://localhost:44336/connect/userinfo"),
+                                n.ProtocolMessage.AccessToken);
+                            var userInfoResponse = await userInfoClient.GetAsync();
+                            var userInfoClaims = userInfoResponse.Claims
+                                .Where(x => x.Item1 != "sub") // filter sub since we're already getting it from id_token
+                                .Select(x => new Claim(x.Item1, x.Item2));
+                            claimsToKeep.AddRange(userInfoClaims);
+                        }
+
+                        var ci = new ClaimsIdentity(
+                            n.AuthenticationTicket.Identity.AuthenticationType,
+                            "name", "role");
+                        ci.AddClaims(claimsToKeep);
+
+                        n.AuthenticationTicket = new Microsoft.Owin.Security.AuthenticationTicket(
+                            ci, n.AuthenticationTicket.Properties
+                        );
+                    },
+                    RedirectToIdentityProvider = n =>
+                    {
+                        if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
+                        {
+                            var idToken = n.OwinContext.Authentication.User.FindFirst("id_token")?.Value;
+                            n.ProtocolMessage.IdTokenHint = idToken;
+                        }
+                        
+                        return Task.FromResult(0);
+                    }
+                }
+            });
+            app.UseStageMarker(PipelineStage.Authenticate);
+        }
+    }
+}
