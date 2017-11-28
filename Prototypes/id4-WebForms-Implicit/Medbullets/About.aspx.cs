e9f64cbefc8c4a1be6b328626f8a52ad3f5fba31
Artyom Shukanov <shukanov.artyom@gmail.com>
2017-11-28 17:06:28 +0300

    Minor fix

--- Prototypes/id4-WebForms-Implicit/Medbullets/About.aspx.cs
+++ Prototypes/id4-WebForms-Implicit/Medbullets/About.aspx.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace WebApp
 {
@@ -11,7 +7,6 @@ namespace WebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
     }
 }
